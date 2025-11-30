5703adc537780673d0598e79d5f5ef70dc493bc4
Guilherme Sales <guilhermedesaales@gmail.com>
2025-11-30 13:00:04 -0300

    mqtt web

--- esp.ino
+++ esp.ino
@@ -3,15 +3,18 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <ESP32Servo.h>
+#include <WiFiClientSecure.h>
 
 // --- CONFIG WIFI / MQTT ---
 const char* ssid = "PC_BatchProcessing";
 const char* password = "05042002";
-const char* mqttServer = "192.168.137.1";
-const int mqttPort = 1883;
+
+const char* mqttServer = "3c16c837ea4f4ac0966899396b41ab08.s1.eu.hivemq.cloud";
+const int mqttPort = 8883;  // TLS
+const char* mqttUser = "guilherme";
+const char* mqttPassword = "Guilherme123";
 
 // --- PINOS ---
-// Vagas
 int sensor1 = 5;
 int ledVermelho1 = 15;
 int ledVerde1 = 4;
@@ -20,13 +23,12 @@ int sensor2 = 26;
 int ledVermelho2 = 18;
 int ledVerde2 = 19;
 
-// Cancela
 int sensorCancela = 2;
 int servoPin = 25;
 
 // --- OBJETOS ---
-WiFiClient espClient;
-PubSubClient client(espClient);
+WiFiClientSecure espClientSecure;
+PubSubClient client(espClientSecure);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 Servo servo;
 
@@ -36,47 +38,40 @@ int estadoAnterior2 = -1;
 String estadoAnteriorPub1 = "";
 String estadoAnteriorPub2 = "";
 
-// Cancela
-int estadoCancela = 0;            
+int estadoCancela = 0;
 unsigned long tempoAbertura = 0;  
 int ultimoValorCancela = HIGH;
-
-// Contador de acessos
 int contadorAcessos = 0;
-
-// Controle de bloqueio
 bool bloqueado1 = false;
 bool bloqueado2 = false;
 
-// LCD
 String lcdLinha1 = "BEM VINDO";
-
-// Piscar LED
 unsigned long tempoUltimoPiscar1 = 0;
 unsigned long tempoUltimoPiscar2 = 0;
 bool estadoLedPiscar1 = false;
 bool estadoLedPiscar2 = false;
 const unsigned long intervaloPiscar = 500;
 
-// Scroll LCD
 unsigned long tempoUltimoScroll1 = 0;
 int scrollIndexLinha1 = 0;
 
-// Flags de conexão
 bool wifiConectado = false;
 bool mqttConectado = false;
 
 // --- FUNÇÕES ---
 void reconnect() {
   while (!client.connected()) {
-    if (client.connect("ESP32Vagas")) {
+    Serial.print("Conectando MQTT...");
+    if (client.connect("ESP32Vagas", mqttUser, mqttPassword)) {
+      Serial.println("Conectado!");
       client.subscribe("estacionamento/vaga1/bloqueio");
       client.subscribe("estacionamento/vaga2/bloqueio");
-      client.subscribe("estacionamento/lcd"); // linha 1
+      client.subscribe("estacionamento/lcd");
       mqttConectado = true;
-      return;
+    } else {
+      Serial.print(".");
+      delay(1000);
     }
-    delay(1000);
   }
 }
 
@@ -86,13 +81,9 @@ void callback(char* topic, byte* payload, unsigned int length){
 
   if(String(topic)=="estacionamento/vaga1/bloqueio") bloqueado1 = (msg=="true");
   if(String(topic)=="estacionamento/vaga2/bloqueio") bloqueado2 = (msg=="true");
-
-  if(String(topic)=="estacionamento/lcd"){
-    lcdLinha1 = msg; // apenas linha 1
-  }
+  if(String(topic)=="estacionamento/lcd") lcdLinha1 = msg;
 }
 
-// Centraliza ou scroll (somente linha 1)
 String getLCDDisplayString(String texto, int &scrollIndex, unsigned long &ultimoScroll) {
   int len = texto.length();
   if(len <= 16){
@@ -120,51 +111,35 @@ String getLCDDisplayString(String texto, int &scrollIndex, unsigned long &ultimo
 void setup() {
   Serial.begin(115200);
 
-  // Vagas
-  pinMode(sensor1, INPUT);
-  pinMode(ledVermelho1, OUTPUT);
-  pinMode(ledVerde1, OUTPUT);
-  pinMode(sensor2, INPUT);
-  pinMode(ledVermelho2, OUTPUT);
-  pinMode(ledVerde2, OUTPUT);
+  pinMode(sensor1, INPUT); pinMode(ledVermelho1, OUTPUT); pinMode(ledVerde1, OUTPUT);
+  pinMode(sensor2, INPUT); pinMode(ledVermelho2, OUTPUT); pinMode(ledVerde2, OUTPUT);
+  pinMode(sensorCancela, INPUT); servo.attach(servoPin); servo.write(0);
 
-  // Cancela
-  pinMode(sensorCancela, INPUT);
-  servo.attach(servoPin);
-  servo.write(0); // cancela fechada
+  lcd.init(); lcd.backlight(); lcd.clear();
+  lcd.setCursor(0,0); lcd.print(lcdLinha1);
+  lcd.setCursor(0,1); lcd.print("Vagas Livres: 00");
 
-  // LCD
-  lcd.init();
-  lcd.backlight();
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(lcdLinha1);
-  lcd.setCursor(0,1);
-  lcd.print("Vagas Livres: 00"); // linha fixa inicial
-
-  // WiFi
   WiFi.begin(ssid, password);
+  Serial.print("Conectando WiFi");
+
+  espClientSecure.setInsecure(); // ignora certificado, necessário para HiveMQ TLS
 }
 
 void loop() {
-  int valor1 = digitalRead(sensor1);
-  int valor2 = digitalRead(sensor2);
-  int valorCancela = digitalRead(sensorCancela);
-
-  // Conexão WiFi
   if(WiFi.status() == WL_CONNECTED && !wifiConectado){
     wifiConectado = true;
+    Serial.println(" - WiFi conectado!");
     client.setServer(mqttServer, mqttPort);
     client.setCallback(callback);
   }
 
-  // Conexão MQTT
-  if(wifiConectado && !mqttConectado){
-    reconnect();
-  }
+  if(wifiConectado && !mqttConectado) reconnect();
   if(mqttConectado) client.loop();
 
-  // Cancela
+  int valor1 = digitalRead(sensor1);
+  int valor2 = digitalRead(sensor2);
+  int valorCancela = digitalRead(sensorCancela);
+
   bool novaDeteccao = (ultimoValorCancela == HIGH && valorCancela == LOW);
   if (estadoCancela == 0 && novaDeteccao) {
     servo.write(90);
@@ -185,7 +160,6 @@ void loop() {
 
   unsigned long now = millis();
 
-  // LEDs Vagas
   if(bloqueado1){
     if(valor1==LOW){
       if(now - tempoUltimoPiscar1 >= intervaloPiscar){
@@ -194,14 +168,8 @@ void loop() {
       }
       digitalWrite(ledVermelho1, estadoLedPiscar1 ? HIGH : LOW);
       digitalWrite(ledVerde1, LOW);
-    } else {
-      digitalWrite(ledVermelho1,HIGH);
-      digitalWrite(ledVerde1,LOW);
-    }
-  } else {
-    digitalWrite(ledVermelho1, valor1==LOW?HIGH:LOW);
-    digitalWrite(ledVerde1, valor1==LOW?LOW:HIGH);
-  }
+    } else { digitalWrite(ledVermelho1,HIGH); digitalWrite(ledVerde1,LOW);}
+  } else { digitalWrite(ledVermelho1, valor1==LOW?HIGH:LOW); digitalWrite(ledVerde1, valor1==LOW?LOW:HIGH); }
 
   if(bloqueado2){
     if(valor2==LOW){
@@ -211,16 +179,9 @@ void loop() {
       }
       digitalWrite(ledVermelho2, estadoLedPiscar2?HIGH:LOW);
       digitalWrite(ledVerde2, LOW);
-    } else {
-      digitalWrite(ledVermelho2,HIGH);
-      digitalWrite(ledVerde2,LOW);
-    }
-  } else {
-    digitalWrite(ledVermelho2, valor2==LOW?HIGH:LOW);
-    digitalWrite(ledVerde2, valor2==LOW?LOW:HIGH);
-  }
+    } else { digitalWrite(ledVermelho2,HIGH); digitalWrite(ledVerde2,LOW);}
+  } else { digitalWrite(ledVermelho2, valor2==LOW?HIGH:LOW); digitalWrite(ledVerde2, valor2==LOW?LOW:HIGH); }
 
-  // Publicação MQTT
   String estado1 = bloqueado1 ? "vaga1:bloqueada" : (valor1==LOW ? "vaga1:ocupada" : "vaga1:livre");
   String estado2 = bloqueado2 ? "vaga2:bloqueada" : (valor2==LOW ? "vaga2:ocupada" : "vaga2:livre");
 
@@ -233,7 +194,6 @@ void loop() {
     estadoAnteriorPub2 = estado2;
   }
 
-  // --- LCD ---
   lcd.setCursor(0,0);
   lcd.print(getLCDDisplayString(lcdLinha1, scrollIndexLinha1, tempoUltimoScroll1));
 
@@ -242,7 +202,7 @@ void loop() {
   if(vagasLivres < 10) linha2 += "0";
   linha2 += String(vagasLivres);
   lcd.setCursor(0,1);
-  lcd.print(linha2); // sempre esquerda, sem scroll
+  lcd.print(linha2);
 
   delay(30);
 }
